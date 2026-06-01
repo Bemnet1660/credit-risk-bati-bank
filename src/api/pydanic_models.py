456ed6a93a883850d2837b09f3c7738a595d5975
BEMNET MULACHEW ANULO <nitababa37@gmail.com>
2026-06-01 13:13:50 +0300

    Update pydanic_models.py

--- src/api/pydanic_models.py
+++ src/api/pydanic_models.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
+from typing import Dict, Any
 
+class CustomerFeatures(BaseModel):
+    features: Dict[str, Any]
+
+class RiskPredictionResponse(BaseModel):
+    risk_probability: float
+    credit_score: int
